1ea1b70f1b578ba74de62d0d81d4260d11d99d76
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2020-07-24 09:34:32 +0200

    add url example

--- test/runtests.jl
+++ test/runtests.jl
@@ -96,3 +96,102 @@ end
     reg3 = at_least(2, not_chars(WHITESPACE, 'a'))
     @test match(reg3, str).match == "kun"
 end
+
+
+@testset "urls" begin
+
+    valid = not_chars("./ -")
+
+    url_pattern = BEGIN *
+        capture(["http" * maybe("s"), "ftp"], as = "protocol") *
+        "://" *
+        maybe(
+            capture(at_least_one(NON_SEPARATOR), as = "username") *
+            maybe(":" * capture(at_least_one(NON_SEPARATOR), as = "password")) *
+            "@"
+        ) *
+        capture(any_of(at_least_one(valid) * maybe("-")) * at_least_one(valid), as = "host") *
+        capture(any_of("." * any_of(at_least_one(valid) * maybe("-")) * at_least_one(valid)), as = "domain") *
+        "." *
+        capture(at_least(2, WORD), as = "TLD") *
+        maybe(":" * capture(between(2, 5, DIGIT), as = "port")) *
+        maybe("/" * capture(at_least_one(NON_SEPARATOR), as = "resource")) *
+        END |> Regex
+
+    goods = (
+        "http://foo.com/blah_blah",
+        "http://foo.com/blah_blah/",
+        "http://foo.com/blah_blah_(wikipedia)",
+        "http://foo.com/blah_blah_(wikipedia)_(again)",
+        "http://www.example.com/wpstyle/?p=364",
+        "https://www.example.com/foo/?bar=baz&inga=42&quux",
+        "http://✪df.ws/123",
+        "http://userid:password@example.com:8080",
+        "http://userid:password@example.com:8080/",
+        "http://userid@example.com",
+        "http://userid@example.com/",
+        "http://userid@example.com:8080",
+        "http://userid@example.com:8080/",
+        "http://userid:password@example.com",
+        "http://userid:password@example.com/",
+        "http://➡.ws/䨹",
+        "http://⌘.ws",
+        "http://⌘.ws/",
+        "http://foo.com/blah_(wikipedia)#cite-1",
+        "http://foo.com/blah_(wikipedia)_blah#cite-1",
+        "http://foo.com/unicode_(✪)_in_parens",
+        "http://foo.com/(something)?after=parens",
+        "http://☺.damowmow.com/",
+        "http://code.google.com/events/#&product=browser",
+        "http://j.mp",
+        "ftp://foo.bar/baz",
+        "http://foo.bar/?q=Test%20URL-encoded%20stuff",
+        "http://مثال.إختبار",
+        "http://例子.测试",
+        "http://-.~_!&'()*+,;=:%40:80%2f::::::@example.com",
+        "http://1337.net",
+        "http://a.b-c.de",
+        "http://223.255.255.254"
+    );
+
+    bads = (
+        "http://",
+        "http://.",
+        "http://..",
+        "http://../",
+        "http://?",
+        "http://??",
+        "http://??/",
+        "http://#",
+        "http://##",
+        "http://##/",
+        "http://foo.bar?q=Spaces should be encoded",
+        "//",
+        "//a",
+        "///a",
+        "///",
+        "http:///a",
+        "foo.com",
+        "rdar://1234",
+        "h://test",
+        "http:// shouldfail.com",
+        ":// should fail",
+        "http://foo.bar/foo(bar)baz quux",
+        "ftps://foo.bar/",
+        "http://-error-.invalid/",
+        "http://-a.b.co",
+        "http://a.b-.co",
+        "http://0.0.0.0",
+        "http://3628126748",
+        "http://.www.foo.bar/",
+        "http://www.foo.bar./",
+        "http://.www.foo.bar./"
+    );
+
+    for url in goods
+        !occursin(url_pattern, url) && println(url)
+    end
+
+    @test all(occursin(url_pattern, url) for url in goods)
+    @test !any(occursin(url_pattern, url) for url in bads)
+end
