7358ab96dbc6c74a758c69945c553e1a6d30feb7
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-06-23 15:26:14 +0200

    add stuff, fix either

--- explain_regex.jl
+++ explain_regex.jl
@@ -22,6 +22,8 @@ specializable = Dict(
 
 rules = Dict(
     :char => P.satisfy(x -> x ∉ special_characters),
+    :namechar => P.satisfy(x -> x in 'a':'z' || x in 'A':'Z'), # more characters allowed?
+    :namestring => P.one_or_more(:namechar),
     :digit => P.satisfy(x -> x in '0':'9'),
     :integer => P.one_or_more(:digit),
     :special_char => P.satisfy(x -> x in special_characters),
@@ -55,37 +57,66 @@ rules = Dict(
         :setcontents,
         :set_close_char,
     ),
-    :sequence => P.one_or_more(:expr),
-    :expr => P.first(:negative_lookahead, :positive_lookahead, :positive_lookbehind, :negative_lookbehind, :zero_or_more, :one_or_more, :repetition, :repetition_at_least, :repetition_from_to, :maybe, :noncapturing_group, :capturing_group, :not_set, :set, :either, :specialized_char, :dot, :normalized_char, :char, :_begin, :_end),
+    :sequence => P.one_or_more(:expr1),
+    :expr1 => P.first(:either, :expr2),
+    :expr2 => P.one_or_more(:basic),
+    :either => P.seq(:expr1, :pipe, :expr2),
+    :basic => P.first(
+        :negative_lookahead,
+        :positive_lookahead,
+        :positive_lookbehind,
+        :negative_lookbehind,
+        :zero_or_more,
+        :one_or_more,
+        :repetition,
+        :repetition_at_least,
+        :repetition_from_to,
+        :maybe,
+        :named_backreference,
+        :named_capturing_group,
+        :noncapturing_group,
+        :capturing_group,
+        :not_set,
+        :set,
+        :specialized_char,
+        :dot,
+        :normalized_char,
+        :char,
+        :_begin,
+        :_end,
+        :either,
+    ),
     :question_mark => P.token('?'),
-    :maybe => P.seq(:expr, :question_mark),
+    :maybe => P.seq(:basic, :question_mark),
     :plus => P.token('+'),
-    :one_or_more => P.seq(:expr, :plus),
+    :one_or_more => P.seq(:basic, :plus),
     :times => P.token('*'),
-    :zero_or_more => P.seq(:expr, :times),
+    :zero_or_more => P.seq(:basic, :times),
     :opening_curly => P.token('{'),
     :closing_curly => P.token('}'),
     :opening_paren => P.token('('),
     :closing_paren => P.token(')'),
     :colon => P.token(':'),
-    :repetition => P.seq(:expr, :opening_curly, :integer, :closing_curly),
-    :repetition_at_least => P.seq(:expr, :opening_curly, :integer, :comma, :closing_curly),
-    :repetition_from_to => P.seq(:expr, :opening_curly, :integer, :comma, :integer, :closing_curly),
+    :repetition => P.seq(:basic, :opening_curly, :integer, :closing_curly),
+    :repetition_at_least => P.seq(:basic, :opening_curly, :integer, :comma, :closing_curly),
+    :repetition_from_to => P.seq(:basic, :opening_curly, :integer, :comma, :integer, :closing_curly),
     :noncapturing_group => P.seq(:opening_paren, :question_mark, :colon, :sequence, :closing_paren),
     :capturing_group => P.seq(:opening_paren, :sequence, :closing_paren),
+    :named_capturing_group => P.seq(:opening_paren, :question_mark, :less, :namestring, :more, :sequence, :closing_paren),
     :comma => P.token(','),
     :pipe => P.token('|'),
-    :either => P.seq(:sequence, :pipe, :sequence),
     :_begin => P.token('^'),
     :_end => P.token('$'),
     :dot => P.token('.'),
     :bang => P.token('!'),
     :equal => P.token('='),
-    :negative_lookahead => P.seq(:expr, :opening_paren, :question_mark, :bang, :sequence, :closing_paren),
-    :positive_lookahead => P.seq(:expr, :opening_paren, :question_mark, :equal, :sequence, :closing_paren),
+    :negative_lookahead => P.seq(:basic, :opening_paren, :question_mark, :bang, :sequence, :closing_paren),
+    :positive_lookahead => P.seq(:basic, :opening_paren, :question_mark, :equal, :sequence, :closing_paren),
     :less => P.token('<'),
-    :positive_lookbehind => P.seq(:opening_paren, :question_mark, :less, :equal, :sequence, :closing_paren, :expr),
-    :negative_lookbehind => P.seq(:opening_paren, :question_mark, :less, :bang, :sequence, :closing_paren, :expr),
+    :more => P.token('>'),
+    :positive_lookbehind => P.seq(:opening_paren, :question_mark, :less, :equal, :sequence, :closing_paren, :basic),
+    :negative_lookbehind => P.seq(:opening_paren, :question_mark, :less, :bang, :sequence, :closing_paren, :basic),
+    :named_backreference => P.seq(:backslash, P.token('k'), :less, :namestring, :more),
 )
 
 g = P.make_grammar(
@@ -109,7 +140,7 @@ function explain_regex(reg::Regex)
 
     expr = P.traverse_match(g, p, i, :sequence,
         fold = function(rule, match, subvals)
-            rule == :sequence ? (length(subvals) == 1 ? only(subvals) : Expr(:call, :*, subvals...)) :
+            rule == :sequence ? collapse_mult(subvals) :
             rule == :char ? only(input[match.pos:match.pos+match.len-1]) :
             rule == :special_char ? only(input[match.pos:match.pos+match.len-1]) :
             rule == :escape_invariant_char ? only(input[match.pos:match.pos+match.len-1]) :
@@ -119,7 +150,9 @@ function explain_regex(reg::Regex)
             rule == :specialized_char ? specializable[subvals[2]] :
             rule == :unescaped_setchar ? only(input[match.pos:match.pos+match.len-1]) :
             rule == :setchar ? subvals[1] :
-            rule == :expr ? subvals[1] :
+            rule == :basic ? subvals[1] :
+            rule == :expr1 ? collapse_mult(subvals) :
+            rule == :expr2 ? collapse_mult(subvals) :
             rule == :chunk ? subvals[1] :
             rule == :set ? Expr(:call, :char_in, subvals[2]...) :
             rule == :not_set ? Expr(:call, :char_not_in, subvals[3]...) :
@@ -136,6 +169,7 @@ function explain_regex(reg::Regex)
             rule == :integer ? parse(Int, String(input[match.pos:match.pos+match.len-1])) :
             rule == :noncapturing_group ? subvals[4] :
             rule == :capturing_group ? Expr(:call, :capture, subvals[2]) :
+            rule == :named_capturing_group ? :(capture($(subvals[6]), as = $(subvals[4]))) :
             rule == :either ? collapse_either(subvals[1], subvals[3]) :
             rule == :_begin ? :BEGIN :
             rule == :_end ? :END :
@@ -144,32 +178,57 @@ function explain_regex(reg::Regex)
             rule == :positive_lookahead ? :(look_for($(subvals[1]), before = $(subvals[5]))) :
             rule == :positive_lookbehind ? :(look_for($(subvals[7]), after = $(subvals[5]))) :
             rule == :negative_lookbehind ? :(look_for($(subvals[7]), not_after = $(subvals[5]))) :
+            rule == :namestring ? String(input[match.pos:match.pos+match.len-1]) :
+            rule == :named_backreference ? :(reference($(subvals[4]))) :
             nothing
     end)
 
-    println("Regex ", reg.pattern)
+    println("Explanation of ", reg.pattern)
+    println("=============================")
     print_formatted(expr)
+
+    expr
 end
 
 function collapse_either(a, b)
-    if b isa Expr && b.head == :call && b.args[1] == :either
-        Expr(:call, :either, a, b.args[2:end]...)
+    if a isa Expr && a.head == :call && a.args[1] == :either
+        Expr(:call, :either, a.args[2:end]..., b)
     else
         Expr(:call, :either, a, b)
     end
 end
 
-explain_regex(r"hi+[^a-z]\??")
+function collapse_mult(subvals)
+    if length(subvals) == 1
+        only(subvals)
+    else
+        args = foldl(subvals; init = []) do l, r
+            if !isempty(l) && r isa Char && (l[end] isa Char || l[end] isa String)
+                l[end] = string(l[end], r)
+            else
+                push!(l, r)
+            end
+            l
+        end
+        if length(args) == 1
+            only(args)
+        else 
+            Expr(:call, :*, args...)
+        end
+    end
+end
+
+@test explain_regex(r"hi+[^a-z]\??") == :('h' * one_or_more('i') * char_not_in('a':1:'z') * maybe('?'))
 explain_regex(r"hi+[^a-z]{8}")
 explain_regex(r"hi+[^a-z]{8,}")
 explain_regex(r"hi+[^a-z]{8,10}")
 explain_regex(r"(?:x?)")
 explain_regex(r"(?:hi+[^a-z]){8,10}")
 explain_regex(r"(?:(?:x))")
-explain_regex(r"hiy*(aa?)")
+@test explain_regex(r"hiy*(aa?)") == :('h' * 'i' * zero_or_more('y') * capture('a' * maybe('a')))
 explain_regex(r"a|bce")
 explain_regex(r"a|b|c|d")
-explain_regex(r"a|b|cde|d")
+explain_regex(r"abc|cd")
 explain_regex(r"[1-9]\d\w\W{0,2}")
 explain_regex(r"^[1-9]$")
 explain_regex(r".?")
@@ -179,4 +238,10 @@ explain_regex(r"^(\$\\)?")
 explain_regex(r"(?<!a)b")
 explain_regex(r"(?<=a)b")
 explain_regex(r"a(?!b)")
-explain_regex(r"a(?=b)")
+explain_regex(r"(^\d*\.?\d*[1-9]+\d*$)|(^[1-9]+\d*\.\d*$)")
+
+@test explain_regex(r"(?<title>xyz)") == :(capture('x' * 'y' * 'z', as = "title"))
+@test explain_regex(r"(?<title>xyz)\k<title>") == :(capture('x' * 'y' * 'z', as = "title") * reference("title"))
+
+explain_regex(r"(?<title>\w+), yes \k<title>");
+explain_regex(r"(^\d*\.?\d*[1-9]+\d*$)");
